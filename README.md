ef396e6c9c703e55f4b70ff717469ca027511bfc
Scott Conway <scott.conway@gmail.com>
2022-04-22 22:01:40 -0500

    first pass - no safeties

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pyWordleSearch
-python script to help me out in wordle
+python script to help me out in wordle.
+
+## Simple flow
+
+The idea is to type in a five letter word then type in the result returned from wordle where X is no match, Y is yellow for that position and G for green. 
+
+### example
+
+Enter word:  atone
+Enter result: xyxxg
+
+then the user will see a list of words that match that pattern and previous patterns as well and will keep looping until the result is all green or the user types exit. 
+
---
+++ pyWordle.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import re
+
+
+def buildPattern(patternList):
+    patternString = ""
+
+    for pattern in patternList:
+        patternString += pattern
+
+    return patternString
+
+
+def removeCharacterFromPattern(pattern, wordCharacter, patternCharacter):
+    if len(pattern) > 1 and patternCharacter != 'g':
+        return pattern.replace(wordCharacter, "")
+
+    return pattern
+
+def updatePattern(index, patternList, wordCharacter, resultCharacter, resultPattern):
+    if resultCharacter == 'x':
+        for i in range(5):
+            patternList[i] = removeCharacterFromPattern(patternList[i], wordCharacter, resultPattern[i])
+
+    if resultCharacter == 'y':
+        value = removeCharacterFromPattern(patternList[index], wordCharacter, resultPattern[index])
+        patternList[index] = value
+
+    if resultCharacter == 'g':
+        patternList[index] = wordCharacter
+
+    return patternList
+
+
+def updatePatternList(patternList, testWord, result):
+    for i in range(5):
+        patternList = updatePattern(i, patternList, testWord[i], result[i], result)
+
+    return patternList
+
+def printPattern(patternList):
+    for pattern in patternList:
+        print(pattern)
+
+def buildWordSet():
+    file = open('fiveLetterWords.txt', "r")
+    wordSet = set()
+
+    for line in file:
+        word = line.strip('\n')
+        wordSet.update({word})
+
+    return wordSet
+
+
+def narrowWordSet(wordSet, pattern):
+    newWordSet = set()
+    for word in wordSet:
+        match = re.match(pattern, word)
+
+        if match:
+            newWordSet.update({word})
+
+    return newWordSet
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Helper program for wordle game.')
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0.0')
+
+    patternList = ["[abcdefghijklmnopqrstuvwxyz]","[abcdefghijklmnopqrstuvwxyz]","[abcdefghijklmnopqrstuvwxyz]","[abcdefghijklmnopqrstuvwxyz]","[abcdefghijklmnopqrstuvwxyz]"]
+    checkPattern = buildPattern(patternList)
+    wordSet = buildWordSet()
+
+    while len(checkPattern) > 5 and len(wordSet) > 1:
+        testWord = ""
+        result = ""
+
+        testWord = input("Word entered in wordle: ")
+        result = input("Enter result from wordle x - no match, y - match wrong position, g - correct letter in correct position: ")
+
+        printPattern(patternList)
+
+        patternList = updatePatternList(patternList, testWord, result)
+        checkPattern = buildPattern(patternList)
+
+        printPattern(patternList)
+
+        wordSet = narrowWordSet(wordSet, checkPattern)
+        print(wordSet)
+
+
+if __name__ == '__main__':
+    main()

991cd44b6b0ea06248e667c4306dd2c297a35a5f
Scott Conway <scott.conway@gmail.com>
2022-04-24 16:48:24 -0500

    Changed all the sets to lists so I can apply my own sorting algorithm.

--- pyWordle.py
+++ pyWordle.py
@@ -3,7 +3,8 @@
 import argparse
 import re
 
-mustHaveCharacters = set()
+mustHaveCharacters = list()
+
 
 def buildPattern(patternList):
     patternString = ""
@@ -20,6 +21,7 @@ def removeCharacterFromPattern(pattern, wordCharacter, patternCharacter):
 
     return pattern
 
+
 def updatePattern(index, patternList, wordCharacter, resultCharacter, resultPattern):
     if resultCharacter == 'x':
         for i in range(5):
@@ -28,11 +30,11 @@ def updatePattern(index, patternList, wordCharacter, resultCharacter, resultPatt
     if resultCharacter == 'y':
         value = removeCharacterFromPattern(patternList[index], wordCharacter, resultPattern[index])
         patternList[index] = value
-        mustHaveCharacters.update({wordCharacter})
+        mustHaveCharacters.append(wordCharacter)
 
     if resultCharacter == 'g':
         patternList[index] = wordCharacter
-        mustHaveCharacters.update({wordCharacter})
+        mustHaveCharacters.append(wordCharacter)
 
     return patternList
 
@@ -43,19 +45,21 @@ def updatePatternList(patternList, testWord, result):
 
     return patternList
 
+
 def printPattern(patternList):
     for pattern in patternList:
         print(pattern)
 
-def buildWordSet(fileName):
+
+def buildWordList(fileName):
     file = open(fileName, "r")
-    wordSet = set()
+    wordList = list()
 
     for line in file:
         word = line.strip('\n')
-        wordSet.update({word})
+        wordList.append(word)
 
-    return wordSet
+    return wordList
 
 
 def hasRequiredCharacters(word):
@@ -69,15 +73,15 @@ def hasRequiredCharacters(word):
     return True
 
 
-def narrowWordSet(wordSet, pattern):
-    newWordSet = set()
-    for word in wordSet:
+def narrowwordList(wordList, pattern):
+    newwordList = list()
+    for word in wordList:
         match = re.match(pattern, word)
 
         if match and hasRequiredCharacters(word):
-            newWordSet.update({word})
+            newwordList.append(word)
 
-    return newWordSet
+    return newwordList
 
 
 def validateWord(testWord):
@@ -106,14 +110,15 @@ def main():
         description='Helper program for wordle game.')
     parser.add_argument('--version', action='version', version='%(prog)s 1.0.0')
 
-    patternList = ["[abcdefghijklmnopqrstuvwxyz]","[abcdefghijklmnopqrstuvwxyz]","[abcdefghijklmnopqrstuvwxyz]","[abcdefghijklmnopqrstuvwxyz]","[abcdefghijklmnopqrstuvwxyz]"]
+    patternList = ["[abcdefghijklmnopqrstuvwxyz]", "[abcdefghijklmnopqrstuvwxyz]", "[abcdefghijklmnopqrstuvwxyz]",
+                   "[abcdefghijklmnopqrstuvwxyz]", "[abcdefghijklmnopqrstuvwxyz]"]
     checkPattern = buildPattern(patternList)
-    wordSet = buildWordSet('fiveLetterWords.txt')
-    commonWordSet = buildWordSet('commonFiveLetterWords.txt')
+    wordList = buildWordList('fiveLetterWords.txt')
+    commonwordList = buildWordList('commonFiveLetterWords.txt')
 
-    while len(checkPattern) > 5 and (len(wordSet) > 1 or len(commonWordSet) > 1):
-        initialWordSetSize = len(wordSet)
-        initialCommonSetSize = len(commonWordSet)
+    while len(checkPattern) > 5 and (len(wordList) > 1 or len(commonwordList) > 1):
+        initialwordListSize = len(wordList)
+        initialCommonSetSize = len(commonwordList)
         testWord = ""
         result = ""
         goodWord = False
@@ -124,7 +129,9 @@ def main():
             goodWord = validateWord(testWord)
 
         while not goodPattern:
-            result = input("Enter result from wordle x - no match, y - match wrong position, g - correct letter in correct position: ")
+            result = input(
+                "Enter result from wordle x - no match, y - match wrong position, g - correct letter in correct "
+                "position: ")
             goodPattern = validatePattern(result)
 
         printPattern(patternList)
@@ -134,15 +141,16 @@ def main():
 
         printPattern(patternList)
 
-        wordSet = narrowWordSet(wordSet, checkPattern)
-        commonWordSet = narrowWordSet(commonWordSet, checkPattern)
-        updatedWordSetSize = len(wordSet)
-        updatedCommonSetSize = len(commonWordSet)
+        wordList = narrowwordList(wordList, checkPattern)
+        commonwordList = narrowwordList(commonwordList, checkPattern)
+        updatedwordListSize = len(wordList)
+        updatedCommonSetSize = len(commonwordList)
 
-        print(f'Your choice has narrowed the full possibilities from {initialWordSetSize} to {updatedWordSetSize}')
-        print(wordSet)
-        print(f'Your choice has narrowed the common possibilities from {initialCommonSetSize} to {updatedCommonSetSize}')
-        print(commonWordSet)
+        print(f'Your choice has narrowed the full possibilities from {initialwordListSize} to {updatedwordListSize}')
+        print(wordList)
+        print(
+            f'Your choice has narrowed the common possibilities from {initialCommonSetSize} to {updatedCommonSetSize}')
+        print(commonwordList)
 
 
 if __name__ == '__main__':

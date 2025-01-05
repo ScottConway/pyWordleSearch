1b4df6a8db1e16f4fca4c481d688e8111e251260
Scott Conway <scott.conway@gmail.com>
2025-01-05 17:11:17 -0600

    initial run of streamlit - still a lot of work to do!

---
+++ runStreamlitPyWordle.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+ /Users/sconway/homebrew/bin/python3.9 -m streamlit run /Users/sconway/PycharmProjects/pyWordleSearch/streamlitWordle.py
---
+++ streamlitWordle.py
@@ -0,0 +1,51 @@
+import argparse
+
+import streamlit as st
+
+import code.EntryList
+
+from code.Entry import Entry
+from code.WordListDirector import WordListDirector
+from code.WordleDisplayHelper import WordleDisplayHelper
+from code.EntryList import EntryList
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Helper program for wordle game.')
+    parser.add_argument('--version', action='version', version='%(prog)s 3.0.3')
+    director = WordListDirector()
+
+    if code.EntryList.entryListInstance is None:
+        code.EntryList.entryListInstance = EntryList()
+
+    finished = False
+
+    st.write("Input word and pattern ie atone-xxyyg")
+    testWord = st.text_input(label='Input word and pattern ie atone-xxyyg', key='inputResult', max_chars=11,
+                           help='Input word and pattern ie atone-xxyyg')
+
+    if testWord:
+        result = testWord[-5:]
+        testWord = testWord[0:5]
+        entry = Entry(testWord, result)
+
+        isValid, errorMessage = code.EntryList.entryListInstance.validateEntry(entry)
+        if isValid:
+            code.EntryList.entryListInstance.add(entry)
+            director.applyEntry(entry)
+            print(code.EntryList.entryListInstance.entries)
+        else:
+            if errorMessage == 'Word already used.':
+                WordleDisplayHelper.printWordAlreadyUsed(entry, code.EntryList.entryListInstance)
+            elif errorMessage == 'Too many must have letters.':
+                WordleDisplayHelper.printTooManyMustHaveLetters(code.EntryList.entryListInstance)
+            else:
+                WordleDisplayHelper.printUnhandledError(errorMessage, entry, code.EntryList.entryListInstance)
+
+
+
+
+
+if __name__ == '__main__':
+    main()

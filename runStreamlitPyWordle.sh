cdacff58d4d70333fde7eae771e2b0eff2d1e68f
Scott Conway <scott.conway@gmail.com>
2025-04-20 22:27:14 -0500

    added paths for mac studio

--- runStreamlitPyWordle.sh
+++ runStreamlitPyWordle.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
- /Users/sconway/homebrew/bin/python3.9 -m streamlit run /Users/sconway/PycharmProjects/pyWordleSearch/streamlitWordle.py
+ #/Users/sconway/homebrew/bin/python3.9 -m streamlit run /Users/sconway/PycharmProjects/pyWordleSearch/streamlitWordle.py
+/opt/homebrew/bin/python3.13 -m streamlit run /Users/scott/Documents/workspace/pyWordleSearch/streamlitWordle.py

e4403fdc17ed8fddc3b7253cd121f11ad5995fee
Scott Conway <scott.conway@gmail.com>
2025-09-20 20:23:01 -0500

    Updates suggested by Cursor code review.

--- code/LetterStatistics.py
+++ code/LetterStatistics.py
@@ -1,13 +1,21 @@
 
 
 class LetterStatistics:
+    """
+    contains the statistics for a single letter in a Wordle game.
+    """
     def __init__(self, count: int, exactCount: bool):
-        super().__init__()
+        """
+        Initialize self.  See help(type(self)) for accurate signature.
 
+        :param count:  Number of occurrences of the letter (0-5).
+        :param exactCount:  if the number of occurrences is EXACT - meaning there is certainty that there are no more
+        occurrences of this letter.
+        """
         if count < 0 or count > 5:
             raise ValueError('Count must be between 0 and 5!')
 
-        self.count = count
-        self.exactCount = exactCount
+        self.count: int = count
+        self.exactCount: bool = exactCount
 
 
--- code/PatternChecker.py
+++ code/PatternChecker.py
@@ -1,15 +1,26 @@
 import re
 class PatternChecker:
+    """
+    This class validates the pattern resulting from an wordle search.
 
+    A valid pattern is five characters long and consists of only g, x, and/or y.
+    """
     @staticmethod
-    def validate(pattern):
+    def validate(pattern: str) -> tuple[bool, str, str]:
+        """
+        Validates a pattern.
+        :param pattern:
+        :return:  goodPattern: True if pattern is valid, False otherwise.
+                  checkedPattern: The pattern passed in for validation - lowercased.
+                  error: an error message if the pattern is invalid.
+        """
         goodPattern = True
         checkedPattern = pattern
         error = ""
 
         match = re.match("^[xygXYG]{5}$", pattern)
         if match is None:
-            error = "The wordle result must be five characters and consist of g, x, or y."
+            error = "The wordle result must be five characters and consist of g, x, or y (upper or lowercased)."
             goodPattern = False
         else:
             checkedPattern = checkedPattern.lower()
